9abb2ffb09a2099cb0a6017dfc39b6b944127d02
Gokussj1722 <44305453+Gokussj1722@users.noreply.github.com>
2020-11-20 23:36:26 +0100

    2222

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,29 +4,17 @@ const { description } = require('../../package')
 module.exports = {
   title: 'FM WORLD Nicaragua',
   //plugins: ['vuepress-plugin-export'],
- /* head: [
+   head: [
     ['link', {rel: 'icon', href: '/favicon.ico'}]
-  ],  */
-  head: [
-    ['link', {rel: 'icon', href: '/favicon.ico'}]/*,
-    ['meta', { name: 'theme-color', content: '#00b3e6' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]*/
   ],
-   //BORRAR EN MULTILENGUAJE
+   
   locales: {
     '/': { lang: 'es-ES'}
       },
- /* MULTILENGUAJE ***RECORDAR CREAR EL DIRECTORIO "EN" PARA PODER ACCEDER AL MULTILENGUAJE***
-  locales: {
-'/': { lang: 'es-ES'},
-'/en/': { lang: 'en-US'}
-  },
-  */
+ 
   //base: '/fmworld-nicaragua/',
    themeConfig: {    
-    docsDir: 'docs',
-    //repo: 'Gokussj1722/FMWORLDNicargua',
+    docsDir: 'docs',    
     logo: '/main.png',
       locales: {
       '/':{
@@ -47,14 +35,7 @@ module.exports = {
     displayAllHeaders: false, // Default: false
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Hombres', link: '/hombres'},
-     /* Para tener una droplist 
-     { text: 'Hombres',
-				items: [          
-          { text: 'Hombres Pure', link: '/hombres'},
-          {	text: 'Hombres Royal',link: '/hombres'}
-        ]
-      },*/
+      { text: 'Hombres', link: '/hombres'},     
       { text: 'Mujeres', link: '/mujer'},
       { text: 'Unisex', link: '/unisex'},
       { text: 'Facebook', link: 'https://www.facebook.com/fmworld.nicaragua'},
@@ -63,37 +44,12 @@ module.exports = {
     ]
   } //QUITAR ESTA LLAVE PARA MULTIIDIOMA
   },
-/*MULTIIDIOMA 
-  '/en/':{
-    selectText: 'Language',
-    label:'English',    
-    sidebar: [
-	     '/en/',
-		 '/en/introduction',
-		 '/en/start',
-		 '/en/objects',
-		 '/en/structure',
-		 '/en/uses',
-		 '/en/appendixa',
-		 '/en/appendixb',
-		 '/en/appendixc'
-	  ],
-    sidebarDepth: 2,
-    displayAllHeaders: false, // Default: false
-    nav: [
-      { text: 'Home', link: '/en/' },
-      { text: 'Objects Dictionary', link: '/en/objects'},
-      { text: 'System Structure', link: '/en/structure'}
-    ]
-  }
-  },
-*/
 
   markdown: {
     // options for markdown-it-anchor
     anchor: { permalink: true },
     // options for markdown-it-toc
-    toc: { includeLevel: [1] },
+    toc: { includeLevel: [1, 2, 3] },
     config: md => {
       // use more markdown-it plugins!
       md.set({html: true})
