46df58255f2a9a6f62b7ddc93ccd5e546dccbfa5
Gokussj1722 <44305453+Gokussj1722@users.noreply.github.com>
2020-11-20 23:48:47 +0100

    TEMA

---
+++ docs/.vuepress/theme/components/Advert.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="ad-spacer"></div>
+
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- vuepress-examples -->
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-8143086736444913"
+        data-ad-slot="7272689166"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  name: 'Advert',
+  data() {
+    return {}
+  },
+  props: [],
+  mixins: [],
+  methods: {},
+  computed: {},
+  watch: {},
+  // beforeCreate() {
+  //   console.log('beforeCreate: Advert')
+  // },
+  // created() {
+  //   console.log('created: Advert')
+  // },
+  // beforeMount() {
+  //   console.log('beforeMount: Advert')
+  // },
+  // mounted() {
+  //   console.log('mounted: Advert')    
+  // },
+  // beforeUpdate() {
+  //   console.log('beforeUpdate: Advert')
+  // },
+  // beforeDestroy() {
+  //   console.log('beforeDestroy: Advert')
+  // },
+  // destroyed() {
+  //   console.log('destroyed: Advert')
+  // }
+}
+</script>
+
+<style lang="stylus">
+
+.adsbygoogle
+  margin-top 4rem !important
+  // background-color red
+
+
+
+</style>
